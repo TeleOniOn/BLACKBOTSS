5996389edbab752122e8ed7c1aefc7c4bc4f4fbb
TeleOniOn <44299448+TeleOniOn@users.noreply.github.com>
2021-03-06 16:37:10 -0800

    Update BLACKBOTSS.lua

--- BLACKBOTSS.lua
+++ BLACKBOTSS.lua
@@ -8320,7 +8320,7 @@ local textchuser = database:get(bot_id..'text:ch:user')
 if textchuser then
 send(msg.chat_id_, msg.id_,'['..textchuser..']')
 else
-send(msg.chat_id_, msg.id_,'⧉️↫عـليك الاشـتࢪاك في قنـاة البـوت اولآ . \n ⧉️↫قنـاة البـوت ←  ['..database:get(bot_id..'add:ch:username')..']')
+send(msg.chat_id_, msg.id_,'⌔︙عـليك الاشـتࢪاك في قنـاة البـوت اولآ . \n ⌔︙قنـاة البـوت ←  ['..database:get(bot_id..'add:ch:username')..']')
 end
 return false
 end
