2cff3427275d09e54251836009cbcc5c1a19b6c1
LOST2014 <80536220+LOST2014@users.noreply.github.com>
2021-03-22 12:45:39 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ chmod +x ts
 get() {
 rm -fr BLACKBOTSS.lua
 rm -fr sudo.lua
-wget "https://raw.githubusercontent.com/Memoz24/BLACKBOTSS/master/BLACKBOTSS.lua"
+wget "https://raw.githubusercontent.com/LOST2014/BLACKBOTSS/master/BLACKBOTSS.lua"
 lua start.lua
 }
 installall(){
